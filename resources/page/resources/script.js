a5a818cf33c2e49ad9b22159b56182dc40363a8a
Josha von Gizycki <mail@joshavg.de>
2019-09-29 21:44:09 +0200

    styling

--- resources/page/resources/script.js
+++ resources/page/resources/script.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    /*$('.gallery img').each(function() {
-        console.debug(this);
-        $(this).wrap('<a href="' + this.src + '"></a>');
-    });*/
     $('.gallery > img').simpleLightbox({
         sourceAttr: 'src'
     });
